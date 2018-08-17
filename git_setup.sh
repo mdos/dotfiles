011a396183b930bf67b271d3247aa4efe7fd5cd2
Marc Dostie <marc.dostie@gmail.com>
2018-08-17 11:02:26 -0400

    Added git setup.sh

---
+++ git_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Setting up git config..."
+
+git config --global user.email "marc.dostie@gmail.com"
+git config --global user.name "Marc Dostie"
+git config --global color.ui "true"
+git config --global alias.s "status"
+git config --global alias.l "log --oneline --graph --pretty=format:\"%C(auto)%h %d %s [%C(dim white)(%cr) %cn%C(reset)]\" --all"
+git config --global alias.co "checkout"
+git config --global core.editor "vim"
+
